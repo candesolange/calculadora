217ebdfa8ea03e7f767ba64c30f575ad5670e4ae
candesolange <calgozzino@gmail.com>
2022-06-27 21:40:49 -0300

    modificaciones

---
+++ index.js
@@ -0,0 +1,4 @@
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonNumeros = document.querySelectorAll('.numero');
+const botonOperador = document.querySelectorAll('.operador');
